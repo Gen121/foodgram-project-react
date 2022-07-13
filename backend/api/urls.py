408985e2fc8af2a1b66153f64773c93235ddc8af
Gen121 <chelnok1190@gmail.com>
2022-07-13 20:00:23 +0300

    Pretested basic user views api.urls configurated

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.views import *
+from api.views import UserViewSet
 
 router = routers.DefaultRouter()
+router.register('users', UserViewSet)
 
 urlpatterns = [
-    # path('', include(router.urls)),
+    path('', include(router.urls)),
 ]
