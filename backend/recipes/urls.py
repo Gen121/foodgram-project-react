67b7efbb01341116fa3eb9a512b71222b7081b5b
Gen121 <chelnok1190@gmail.com>
2022-07-13 19:08:45 +0300

    Deleted unused files

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
-from .views import index
-
 app_name = 'recipes'
 
 urlpatterns = [
-    path('tag/', index, name='index'),
+    # path('tag/', index, name='index'),
 ]
--- backend/recipes/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import HttpResponse, get_object_or_404
-
-# from .models import Parent
-
-
-def index(request):
-    # p = get_object_or_404(Parent, slug='adam').slug
-    return HttpResponse(
-        f'{1+1}')
