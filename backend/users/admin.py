aa2d15f4f3658f2364ff310e80ae5f3b4cce4915
Gen121 <chelnok1190@gmail.com>
2022-07-14 18:17:22 +0300

    Admin register user model

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 
-from .models import Profile
+from .models import Profile, User
 
-admin.site.unregister(User)
 admin.site.register(Profile)
 
 
