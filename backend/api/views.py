a1f6b753b7ae4251997c322aa53da04fed72db0d
Gen121 <chelnok1190@gmail.com>
2022-07-14 18:14:37 +0300

    Added viesets for Tag and Ingredient

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,18 +1,28 @@
+from djoser.serializers import TokenSerializer
 from rest_framework import mixins, viewsets
 
-from users.models import User
-from api.serializers import UserSerializer
+from api.serializers import TagSerializer, IngredientSerializer
+# from api.serializers import IngredientInRecipeSerializer
+from recipes.models import Tag, Ingredient
+# from recipes.models import IngredientInRecipe
 
 
-class CreateListDestroyViewSet(mixins.CreateModelMixin,
-                               mixins.ListModelMixin,
-                               viewsets.GenericViewSet, ):
-    lookup_field = 'id'
-    # # permission_classes = (AdminOrReadOnnly, )
-    # # filter_backends = (filters.SearchFilter, )
-    # search_fields = ('name', )
+class ListRetrieveViewSet(mixins.ListModelMixin,
+                          mixins.RetrieveModelMixin,
+                          viewsets.GenericViewSet, ):
+    pass
 
 
-class UserViewSet(CreateListDestroyViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class TagViewSet(ListRetrieveViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class IngredientViewSet(ListRetrieveViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+
+# class IngredientInRecipeViewSet(ListRetrieveViewSet):
+#     queryset = IngredientInRecipe.objects.all()
+#     serializer_class = IngredientInRecipeSerializer
