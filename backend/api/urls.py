87d2b327a5ea74138ebfa18772fe40b316107a80
Gen121 <chelnok1190@gmail.com>
2022-07-14 18:04:53 +0300

    Register routers for Tag and Ingridients

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,19 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.views import UserViewSet
+from api.views import TagViewSet, IngredientViewSet
+# from api.views import IngredientInRecipe
+
+app_name = 'api'
 
 router = routers.DefaultRouter()
-router.register('users', UserViewSet)
+router.register('tags', TagViewSet, basename='tags')
+router.register(r'tags/(?P<tag_id>[0-9]+)', TagViewSet, basename='tag_detail')
+router.register('ingredients', IngredientViewSet, basename='ingredients')
+router.register(r'ingredients/(?P<tag_id>[0-9]+)',
+                IngredientViewSet,
+                basename='ingredient_detail', )
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router.urls), ),
 ]
