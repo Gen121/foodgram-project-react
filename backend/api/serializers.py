f2dc14d208499e64b761aed1fa0c9abedc413468
Gen121 <chelnok1190@gmail.com>
2022-07-14 18:10:48 +0300

    Add serializers for Tag and Ingridients

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,20 +1,46 @@
 from django.core.exceptions import ValidationError
 from django.shortcuts import get_object_or_404
-from recipes.models import Ingredient, IngredientInRecipe, Recipe, Tag
 from rest_framework import serializers
-from shopping_cart.models import ShoppingCart
-from users.models import Profile, User
+from djoser.serializers import UserSerializer
+
+from recipes.models import Ingredient, Recipe, IngredientInRecipe, Tag
 
 
-class UserSerializer(serializers.ModelSerializer):
-    is_subscribed = serializers.SerializerMethodField()
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('id', 'name', 'color', 'slug', )
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'name', 'measurement_unit', )
+
+
+class IngredientInRecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IngredientInRecipe
+        fields = ('id', 'name', 'measurement_unit', 'amount', )
+
+
+class RetrieveRecipeSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True, read_only=True)
+    author = UserSerializer(read_only=True)
+    ingredients = IngredientInRecipeSerializer(many=True, read_only=True)
 
     class Meta:
-        model = User
-        fields = (
-            'email', 'id', 'username',  'first_name', 'last_name',
-            # 'is_subscribed',
-            )
+        model = Recipe
+        fields = ('id', 'tags', 'author', 'ingredients',
+                  'name', 'description', )
 
-    # def get_is_subscribed(self, user_id):
-    #     return obj.is_subscribed
+    @property
+    def get_author(self):
+        return self.context['request'].user
+
+    @property
+    def get_recipe_id(self):
+        return self.context['request'].parser_context['kwargs']['pk']
+
+    def get_is_favorited(self, recipe):
+        return recipe.is_favored(self.get_author)
