a31be92859f039aa90b4462692a4c00601cbeb07
Gen121 <chelnok1190@gmail.com>
2022-07-15 18:45:07 +0300

    Register RecipeViewSet to router api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.views import TagViewSet, IngredientViewSet
+from api.views import TagViewSet, IngredientViewSet, RecipeViewSet
 # from api.views import IngredientInRecipe
 
 app_name = 'api'
@@ -11,8 +11,10 @@ router.register('tags', TagViewSet, basename='tags')
 router.register(r'tags/(?P<tag_id>[0-9]+)', TagViewSet, basename='tag_detail')
 router.register('ingredients', IngredientViewSet, basename='ingredients')
 router.register(r'ingredients/(?P<tag_id>[0-9]+)',
-                IngredientViewSet,
-                basename='ingredient_detail', )
+                IngredientViewSet, basename='ingredient_detail', )
+router.register('recipes', RecipeViewSet, basename='recipes')
+router.register(r'recipes/(?P<recipe_id>[0-9]+)',
+                RecipeViewSet, basename='recipe_detail')
 
 urlpatterns = [
     path('', include(router.urls), ),
