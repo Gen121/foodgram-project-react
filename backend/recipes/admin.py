f580b1ef3a5810e0fdd556d85b8d8faac4d51fcb
Gen121 <chelnok1190@gmail.com>
2022-07-15 17:04:33 +0300

    Added inline fields to Recipe model in admin site

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
 
 from recipes.models import Ingredient, IngredientInRecipe, Recipe, Tag
+from users.models import Profile, User
 
 admin.site.register(Ingredient)
 admin.site.register(IngredientInRecipe)
-admin.site.register(Recipe)
 admin.site.register(Tag)
+
+
+class RecipeIngredientsInline(admin.TabularInline):
+    model = IngredientInRecipe
+    extra = 1
+    min_num = 1
+    fk_name = 'recipe'
+    verbose_name = 'Ингредиент'
+    verbose_name_plural = 'Ингредиенты'
+
+
+class TagInline(admin.TabularInline):
+    model = Recipe.tags.through
+    extra = 0
+    fk_name = 'recipe'
+    verbose_name = 'Тег'
+    verbose_name_plural = 'Теги'
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    fields = ('name', 'text', 'image', 'author', 'cooking_time', )
+    inlines = [RecipeIngredientsInline, TagInline, ]
