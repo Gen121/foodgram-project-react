835c962fc7e3d11c8eec519ed2583e4a19c75420
Gen121 <chelnok1190@gmail.com>
2022-07-18 20:12:38 +0300

    Fixed  api urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.views import TagViewSet, IngredientViewSet, RecipeViewSet
-# from api.views import IngredientInRecipe
+from api.views import IngredientViewSet, RecipeViewSet, TagViewSet
 
 app_name = 'api'
 
