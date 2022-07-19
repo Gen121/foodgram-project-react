bd601edf4484f75c8f94b6aa397bd807be8d90e2
Gen121 <chelnok1190@gmail.com>
2022-07-19 20:18:38 +0300

    Add Djoser routes to api urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,20 +1,28 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.views import IngredientViewSet, RecipeViewSet, TagViewSet
+from api.views import (FollowingViewSet, IngredientViewSet, RecipeViewSet,
+                       TagViewSet, UserViewSet)
 
 app_name = 'api'
 
 router = routers.DefaultRouter()
+
 router.register('tags', TagViewSet, basename='tags')
 router.register(r'tags/(?P<tag_id>[0-9]+)', TagViewSet, basename='tag_detail')
+
 router.register('ingredients', IngredientViewSet, basename='ingredients')
 router.register(r'ingredients/(?P<tag_id>[0-9]+)',
                 IngredientViewSet, basename='ingredient_detail', )
+
 router.register('recipes', RecipeViewSet, basename='recipes')
 router.register(r'recipes/(?P<recipe_id>[0-9]+)',
                 RecipeViewSet, basename='recipe_detail')
 
+router.register('users', UserViewSet, basename='users')
+
+
 urlpatterns = [
     path('', include(router.urls), ),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
