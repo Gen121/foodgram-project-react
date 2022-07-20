1a59941c01c7b6f1e89575a158399195995f65de
Gen121 <chelnok1190@gmail.com>
2022-07-20 15:30:02 +0300

    Fix urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
@@ -10,9 +9,3 @@ urlpatterns = [
          name='redoc'),
     path('api/', include('api.urls')),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls))
-    ] + urlpatterns
