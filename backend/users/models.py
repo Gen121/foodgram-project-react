1ab6d6be6a28232d3bce652ddd4002442998078a
Gen121 <chelnok1190@gmail.com>
2022-07-22 13:31:40 +0300

    Added migrations

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -29,8 +28,6 @@ class User(AbstractUser):
         ordering = ['username', ]
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
-
-
 
     def __str__(self):
         return self.username
