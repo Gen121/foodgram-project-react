57a8922ffc0f5b35c10f31b4cd2fe18f33dd9ccf
Gen121 <chelnok1190@gmail.com>
2022-07-25 01:08:23 +0300

    Append validate by forbidden user name

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,8 +1,22 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from users.validators import CustomUsernameValidator
+from django.utils.translation import gettext_lazy as _
+
 
 class User(AbstractUser):
+    username_validator = CustomUsernameValidator()
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
     email = models.EmailField(
         max_length=254,
         unique=True,
@@ -76,8 +90,7 @@ class Favorite(models.Model):
         verbose_name_plural = 'Избранные рецепты'
         constraints = [
             models.UniqueConstraint(fields=['user', 'recipe'],
-                                    name='unique_favorite_recipe')
-        ]
+                                    name='unique_favorite_recipe'), ]
 
     def __str__(self):
         return f"{self.recipe} in {self.user}'s favorites"
--- backend/users/validators.py
+++ backend/users/validators.py
@@ -1,12 +1,19 @@
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.core.exceptions import ValidationError
 
-UNCORRECT_NAME = [
+UNCORRECT_NAME = {
     'me',
     'login',
     'username',
-]
+}
 
 
-def validate_username(username):
+def custom_username_validator(username):
     if username in UNCORRECT_NAME:
         raise ValidationError(f'{username} - Недоступное имя пользователя.')
+
+
+class CustomUsernameValidator(UnicodeUsernameValidator):
+    def __call__(self, value):
+        custom_username_validator(value)
+        return super().__call__(value)
