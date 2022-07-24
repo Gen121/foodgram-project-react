ad2af0eefc1ae5dd20cb905c1efddedb64947d0f
Gen121 <chelnok1190@gmail.com>
2022-07-24 23:40:31 +0300

    Added deafult pagination class for api

--- backend/api/paginations.py
+++ backend/api/paginations.py
@@ -1,6 +1,7 @@
+from django.conf import settings
 from rest_framework.pagination import PageNumberPagination
 
 
 class PagePagination(PageNumberPagination):
-    page_size = 6
+    page_size = settings.REST_FRAMEWORK['PAGE_SIZE']
     page_size_query_param = 'limit'
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -143,6 +143,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 6,
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
