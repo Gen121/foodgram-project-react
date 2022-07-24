ee3f81d10bb789d5ff1460dc4591de427c57e6b6
Gen121 <chelnok1190@gmail.com>
2022-07-25 01:00:32 +0300

    Replace api urls to top urlpatterns

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,9 +3,9 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
     path('redoc/',
          TemplateView.as_view(template_name='redoc.html'),
          name='redoc'),
-    path('api/', include('api.urls')),
 ]
