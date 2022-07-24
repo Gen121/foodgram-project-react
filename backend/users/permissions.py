230bb455fe1aa2bc519176416c10cbed7a5ddde2
Gen121 <chelnok1190@gmail.com>
2022-07-25 01:05:46 +0300

    Fix permissions by PEP8

--- backend/users/permissions.py
+++ backend/users/permissions.py
@@ -6,6 +6,7 @@ class IsAuthorOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method not in SAFE_METHODS:
             return obj.author == request.user
+
         return True
 
 
@@ -14,5 +15,5 @@ class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
-        else:
-            return request.user.is_staff
+
+        return request.user.is_staff
